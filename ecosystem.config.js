367a6cd85bce7287d988db42b7d6b1bae51675d1
Kevin Medeiros <108627831+medeirosnvk@users.noreply.github.com>
2024-04-04 10:54:47 -0300

    wip

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,6 @@ module.exports = {
       name: "app-santander",
       script: "yarn",
       args: "dev", // Execute o script "dev" definido em package.json usando npm
-      interpreter: "/bin/bash", // Caminho do interpretador
     },
   ],
 };
