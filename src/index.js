dfcede2c4f3e4a3b6d6260682b32eeb5421bd6df
Kevin Medeiros <108627831+medeirosnvk@users.noreply.github.com>
2024-04-04 10:22:17 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,50 @@
+import * as dotenv from "dotenv";
+import express from "express";
+import fs from "fs";
+import path from "path";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.json());
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
+app.post("/santander", (req, res) => {
+  const data = req.body;
+  console.log(data);
+
+  if (Object.keys(data).length === 0) {
+    return res.status(400).json({ error: "Nenhum dado fornecido" });
+  }
+
+  try {
+    const folderPath = path.join(__dirname, "src", "data");
+
+    if (!fs.existsSync(folderPath)) {
+      fs.mkdirSync(folderPath, { recursive: true });
+    }
+
+    const timestamp = new Date().toISOString().replace(/:/g, "-");
+    const fileName = `${timestamp}.json`;
+    const filePath = path.join(folderPath, fileName);
+
+    fs.writeFile(filePath, JSON.stringify(data), (err) => {
+      if (err) {
+        console.error("Erro ao gravar os dados:", err);
+        return res.status(500).json({ error: "Erro ao gravar os dados" });
+      }
+      console.log("Dados gravados com sucesso!");
+      res.status(201).json({ fileName }); // Sending the generated filename back to the client
+    });
+  } catch (error) {
+    console.error("Erro ao escrever no arquivo:", error);
+    res.status(500).json({ error: "Erro ao escrever no arquivo" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Backend iniciado na porta ${port}`);
+});
